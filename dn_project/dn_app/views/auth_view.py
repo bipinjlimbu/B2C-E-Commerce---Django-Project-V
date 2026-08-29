0ace010c8fdf05d0e6418f7e81d6b205be6d5783
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-29 22:33:30 +0545

    Login Page Logic Added

--- dn_project/dn_app/views/auth_view.py
+++ dn_project/dn_app/views/auth_view.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from ..models import User
 
 def login_view(request):
+    errors = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if not username:
+            errors['username'] = 'Username is required.'
+        if not password:
+            errors['password'] = 'Password is required.'
+
+        if errors:
+            return render(request, 'auth/login_page.html', {'data':request.POST,'errors': errors})
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            
     return render(request, 'auth/login_page.html')
