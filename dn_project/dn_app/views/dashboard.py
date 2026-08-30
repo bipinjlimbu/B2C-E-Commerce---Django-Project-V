b67e7ed6225796c4012a22c020fb2214e7579886
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-30 18:42:55 +0545

    Customer Management Section Data Fetched

--- dn_project/dn_app/views/dashboard.py
+++ dn_project/dn_app/views/dashboard.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from ..models import User
 
 @login_required
 def admin_dashboard_view(request):
@@ -15,7 +16,7 @@ def admin_dashboard_view(request):
     }
     
     if section == 'customer-management':
-        context['customers'] = None
+        context['customers'] = User.objects.filter(is_superuser=False)
     
     if section == 'product-management':
         context['products'] = None
