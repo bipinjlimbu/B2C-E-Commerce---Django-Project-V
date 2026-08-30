9d601e818f986eaf0af7b962f573e1d13c7b954d
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-30 18:04:09 +0545

    Edit Profile Logic Added

--- dn_project/dn_app/views/profile_view.py
+++ dn_project/dn_app/views/profile_view.py
@@ -9,4 +9,41 @@ def profile_view(request):
 
 @login_required
 def edit_profile_view(request):
+    errors = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        profile_picture = request.FILES.get('profile_picture')
+
+        if not username:
+            errors['username'] = 'Username is required.'
+        elif User.objects.filter(username=username).exclude(id=request.user.id).exists():
+            errors['username'] = 'Username already exists.'
+            
+        if not email:
+            errors['email'] = 'Email is required.'
+        elif User.objects.filter(email=email).exclude(id=request.user.id).exists():
+            errors['email'] = 'Email already exists.'
+
+        if errors:
+            return render(request, 'main/edit_profile_page.html', {'data':request.POST,'errors': errors})
+
+        user = User.objects.get(id=request.user.id)
+        user.username = username
+        user.email = email
+        user.first_name = first_name
+        user.last_name = last_name
+        user.phone = phone
+        user.address = address
+        if profile_picture:
+            user.profile_picture = profile_picture
+        user.save()
+        
+        messages.success(request, 'Profile updated successfully.')
+        return redirect('/profile/')
+    
     return render(request, 'main/edit_profile_page.html')
