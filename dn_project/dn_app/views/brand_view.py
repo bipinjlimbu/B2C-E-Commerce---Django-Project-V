96f59f18bd1cc2f413644fedb8bd718d1973a3fc
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-30 19:15:33 +0545

    Add Brand Logic Added

--- dn_project/dn_app/views/brand_view.py
+++ dn_project/dn_app/views/brand_view.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from ..models import Brand
 
 @login_required
 def add_brand_view(request):
+    errors = {}
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        logo = request.FILES.get('logo')
+        
+        if not name:
+            errors['name'] = 'Brand name is required.'
+        elif Brand.objects.filter(name=name).exists():
+            errors['name'] = 'Brand name already exists.'
+            
+        if not logo:
+            errors['logo'] = 'Brand logo is required.'
+            
+        if errors:
+            return render(request, 'main/add_brand_page.html', {'data':request.POST,'errors': errors})
+        
+        brand = Brand(name=name, logo=logo)
+        brand.save()
+        messages.success(request, 'Brand added successfully.')
+        return redirect('/dashboard/admin/?section=brand-management')
+    
     return render(request, 'main/add_brand_page.html')
