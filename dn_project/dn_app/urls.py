da6e277977e96f1e73486588989e9ecd22222763
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-30 17:59:18 +0545

    Profile Page Added

--- dn_project/dn_app/urls.py
+++ dn_project/dn_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views.auth_view import login_view, logout_view, register_view
 from .views.main_view import home_view
+from .views.profile_view import profile_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
+    path('profile/', profile_view, name='profile'),
 ]
