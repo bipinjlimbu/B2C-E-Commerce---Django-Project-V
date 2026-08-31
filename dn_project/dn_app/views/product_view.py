a73b516d08166797b8a3b0a669d879cff8cf23b4
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-31 18:24:14 +0545

    Edit Product Logic Updated

--- dn_project/dn_app/views/product_view.py
+++ dn_project/dn_app/views/product_view.py
@@ -70,4 +70,65 @@ def add_product_view(request):
 
 @login_required
 def edit_product_view(request, product_id):
-    return render(request, 'main/edit_product_page.html', {'product_id': product_id})
+    product = Product.objects.get(id=product_id)
+    brands = Brand.objects.all()
+    
+    errors = {}
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        brand_id = request.POST.get('brand')
+        category = request.POST.get('category')
+        skin_type = request.POST.get('skin_type')
+        volume = request.POST.get('volume')
+        ingredients = request.POST.get('ingredients')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        stock = request.POST.get('stock')
+        sku = request.POST.get('sku')
+        product_image = request.FILES.get('product_image')
+        is_active = request.POST.get('is_active') == 'true'
+
+        if not name:
+            errors['name'] = 'Product name is required.'
+        if not brand_id:
+            errors['brand'] = 'Brand is required.'
+        if not category:
+            errors['category'] = 'Category is required.'
+        if not volume:
+            errors['volume'] = 'Volume is required.'
+        if not description:
+            errors['description'] = 'Description is required.'
+        if not price:
+            errors['price'] = 'Price is required.'
+        if not stock:
+            errors['stock'] = 'Stock is required.'
+            
+        if not sku:
+            errors['sku'] = 'SKU is required.'
+        elif Product.objects.filter(sku=sku).exclude(id=product_id).exists():
+            errors['sku'] = 'SKU already exists.'
+
+        if errors:
+            return render(request, 'main/edit_product_page.html', {'product': product, 'data':request.POST,'errors': errors,'brands': brands})
+
+        brand = Brand.objects.get(id=brand_id)
+        
+        product.name = name
+        product.brand = brand
+        product.category = category
+        product.skin_type = skin_type
+        product.volume = volume
+        product.ingredients = ingredients
+        product.description = description
+        product.price = price
+        product.stock = stock
+        product.sku = sku
+        if product_image:
+            product.product_image = product_image
+        product.is_active = is_active
+        
+        product.save()
+        messages.success(request, 'Product updated successfully.')
+        return redirect('/dashboard/admin/?section=product-management')
+    
+    return render(request, 'main/edit_product_page.html', {'product': product, 'brands': brands})
