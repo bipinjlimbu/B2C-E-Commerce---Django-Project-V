e24fe9899c7e3c8f28f17c27260e95f5119f7bd4
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-31 18:08:00 +0545

    Add Product Logic Added

--- dn_project/dn_app/views/product_view.py
+++ dn_project/dn_app/views/product_view.py
@@ -5,4 +5,56 @@ from ..models import Product, Brand
 
 @login_required
 def add_product_view(request):
-    return render(request, 'main/add_product_page.html')
+    errors = {}
+    brands = Brand.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        brand_id = request.POST.get('brand')
+        category = request.POST.get('category')
+        skin_type = request.POST.get('skin_type')
+        volume = request.POST.get('volume')
+        ingredients = request.POST.get('ingredients')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        stock = request.POST.get('stock')
+        product_image = request.FILES.get('product_image')
+        is_active = request.POST.get('is_active') == 'true'
+
+        if not name:
+            errors['name'] = 'Product name is required.'
+        if not brand_id:
+            errors['brand'] = 'Brand is required.'
+        if not category:
+            errors['category'] = 'Category is required.'
+        if not volume:
+            errors['volume'] = 'Volume is required.'
+        if not description:
+            errors['description'] = 'Description is required.'
+        if not price:
+            errors['price'] = 'Price is required.'
+        if not stock:
+            errors['stock'] = 'Stock is required.'
+        if not product_image:
+            errors['product_image'] = 'Product image is required.'
+
+        if errors:
+            return render(request, 'main/add_product_page.html', {'data':request.POST,'errors': errors,'brands': brands})
+
+        brand = Brand.objects.get(id=brand_id)
+        product = Product(
+            name=name,
+            brand=brand,
+            category=category,
+            skin_type=skin_type,
+            volume=volume,
+            ingredients=ingredients,
+            description=description,
+            price=price,
+            stock=stock,
+            product_image=product_image
+        )
+        product.save()
+        messages.success(request, 'Product added successfully.')
+        return redirect('/dashboard/admin/?section=product-management')
+    
+    return render(request, 'main/add_product_page.html', {'brands': brands})
