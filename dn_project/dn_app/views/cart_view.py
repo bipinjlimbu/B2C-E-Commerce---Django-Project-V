53bb86a48e8fb22068ca0aa96e7dd41f4c59c5cb
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-31 19:49:29 +0545

    Increase Cart Item Quantity Logic Added

--- dn_project/dn_app/views/cart_view.py
+++ dn_project/dn_app/views/cart_view.py
@@ -40,3 +40,29 @@ def cart_view(request):
     total_price = sum(item.product.price * item.quantity for item in cart_items)
 
     return render(request, 'main/cart_page.html', {'cart_items': cart_items, 'total_price': total_price})
+
+@login_required
+def increase_cart_item_quantity_view(request, cart_item_id):
+    if not request.user.is_authenticated and request.user.is_superuser:
+        messages.error(request, 'You are not authorized to modify the cart.')
+        return redirect('/')
+    
+    cart_item = CartItem.objects.get(id=cart_item_id)
+    
+    if not cart_item:
+        messages.error(request, 'Cart item does not exist.')
+        return redirect('/cart/')
+    
+    if cart_item.cart.customer != request.user:
+        messages.error(request, 'You are not authorized to modify this cart item.')
+        return redirect('/cart/')
+    
+    if cart_item.quantity >= cart_item.product.stock:
+        messages.error(request, 'Cannot increase quantity. Not enough stock available.')
+        return redirect('/cart/')
+    
+    cart_item.quantity += 1
+    cart_item.save()
+    messages.success(request, 'Product quantity increased in the cart.')
+        
+    return redirect('/cart/')
