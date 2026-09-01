e0833604d4a53d284a8b5ab7f88ce7c7a4b8b748
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-01 19:29:11 +0545

    Deliver Order Logic Added

--- dn_project/dn_app/views/order_view.py
+++ dn_project/dn_app/views/order_view.py
@@ -22,3 +22,23 @@ def dispatch_order_view(request, order_id):
     order.save()
     messages.success(request, 'Order dispatched successfully.')
     return redirect('/dashboard/admin/?section=order-fulfillment')
+
+@login_required
+def deliver_order_view(request, order_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'You are not authorized to access this page.')
+        return redirect('/')
+    
+    order = Order.objects.get(id=order_id)
+    if not order:
+        messages.error(request, 'Order does not exist.')
+        return redirect('/dashboard/admin/?section=order-fulfillment')
+    
+    if order.status != Order.Status.SHIPPING:
+        messages.error(request, 'Only orders in shipping status can be marked as delivered.')
+        return redirect('/dashboard/admin/?section=order-fulfillment')
+    
+    order.status = Order.Status.DELIVERED
+    order.save()
+    messages.success(request, 'Order marked as delivered successfully.')
+    return redirect('/dashboard/admin/?section=order-fulfillment')
